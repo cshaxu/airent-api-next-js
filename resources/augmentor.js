fd7a04d3827835ccba51b3e102f222bb59d9adc3
cshaxu <cshaxu@gmail.com>
2024-01-04 11:35:45 -0800

    [0.1.4] update api path

--- resources/augmentor.js
+++ resources/augmentor.js
@@ -6,11 +6,11 @@ function augmentOne(entity, config, utils) {
   const kababPluralEntityName = utils.toKababCase(utils.pluralize(entity.name));
   const serverApiOutputPath = `src/app${config.apiPath}/${kababPluralEntityName}`;
   const serverApiOutputs = {
-    "node_modules/airent-api-next/resources/server-create-one-template.ts.ejs": `${serverApiOutputPath}/createOne/route.ts`,
-    "node_modules/airent-api-next/resources/server-delete-one-template.ts.ejs": `${serverApiOutputPath}/deleteOne/route.ts`,
-    "node_modules/airent-api-next/resources/server-get-many-template.ts.ejs": `${serverApiOutputPath}/getMany/route.ts`,
-    "node_modules/airent-api-next/resources/server-get-one-template.ts.ejs": `${serverApiOutputPath}/getOne/route.ts`,
-    "node_modules/airent-api-next/resources/server-update-one-template.ts.ejs": `${serverApiOutputPath}/updateOne/route.ts`,
+    "node_modules/airent-api-next/resources/server-create-one-template.ts.ejs": `${serverApiOutputPath}/create-one/route.ts`,
+    "node_modules/airent-api-next/resources/server-delete-one-template.ts.ejs": `${serverApiOutputPath}/delete-one/route.ts`,
+    "node_modules/airent-api-next/resources/server-get-many-template.ts.ejs": `${serverApiOutputPath}/get-many/route.ts`,
+    "node_modules/airent-api-next/resources/server-get-one-template.ts.ejs": `${serverApiOutputPath}/get-one/route.ts`,
+    "node_modules/airent-api-next/resources/server-update-one-template.ts.ejs": `${serverApiOutputPath}/update-one/route.ts`,
   };
 
   entity.outputs = { ...entity.outputs, ...serverApiOutputs };
