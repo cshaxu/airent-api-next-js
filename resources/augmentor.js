3cffc5a949069d69971e2e62bd21ca37972e7b37
cshaxu <cshaxu@gmail.com>
2024-01-08 16:23:59 -0800

    [0.1.4] update api path

--- resources/augmentor.js
+++ resources/augmentor.js
@@ -3,14 +3,15 @@ function augmentOne(entity, config, utils) {
     return;
   }
 
+  const kababSingularEntityName = utils.toKababCase(entity.name);
   const kababPluralEntityName = utils.toKababCase(utils.pluralize(entity.name));
-  const serverApiOutputPath = `${config.nextApiSourcePath}/${config.apiPath}/${kababPluralEntityName}`;
+  const serverApiOutputPath = `${config.nextApiSourcePath}/${config.apiPath}`;
   const serverApiOutputs = {
-    "node_modules/airent-api-next/resources/server-create-one-template.ts.ejs": `${serverApiOutputPath}/create-one/route.ts`,
-    "node_modules/airent-api-next/resources/server-delete-one-template.ts.ejs": `${serverApiOutputPath}/delete-one/route.ts`,
-    "node_modules/airent-api-next/resources/server-get-many-template.ts.ejs": `${serverApiOutputPath}/get-many/route.ts`,
-    "node_modules/airent-api-next/resources/server-get-one-template.ts.ejs": `${serverApiOutputPath}/get-one/route.ts`,
-    "node_modules/airent-api-next/resources/server-update-one-template.ts.ejs": `${serverApiOutputPath}/update-one/route.ts`,
+    "node_modules/airent-api-next/resources/server-get-many-template.ts.ejs": `${serverApiOutputPath}/get-many-${kababPluralEntityName}/route.ts`,
+    "node_modules/airent-api-next/resources/server-get-one-template.ts.ejs": `${serverApiOutputPath}/get-one-${kababSingularEntityName}/route.ts`,
+    "node_modules/airent-api-next/resources/server-create-one-template.ts.ejs": `${serverApiOutputPath}/create-one-${kababSingularEntityName}/route.ts`,
+    "node_modules/airent-api-next/resources/server-delete-one-template.ts.ejs": `${serverApiOutputPath}/delete-one-${kababSingularEntityName}/route.ts`,
+    "node_modules/airent-api-next/resources/server-update-one-template.ts.ejs": `${serverApiOutputPath}/update-one-${kababSingularEntityName}/route.ts`,
   };
 
   entity.outputs = { ...entity.outputs, ...serverApiOutputs };
